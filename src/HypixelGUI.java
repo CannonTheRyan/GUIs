c3d6b7da390e8f06e45b83c7779382e964936979
BT_1E10_33 <sample@bths.edu>
2022-05-17 10:59:09 -0400

    Initial commit

---
+++ src/HypixelGUI.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class HypixelGUI implements ActionListener
+{
+
+
+    public HypixelGUI()
+    {
+
+    }
+
+    public void load()
+    {
+        JFrame frame = new JFrame();
+        ImageIcon logo = new ImageIcon("src/HypixelLogo.jpg");
+        Image logoData = logo.getImage();
+        JPanel panel = new JPanel();
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        HypixelGUI gui = new HypixelGUI();
+        gui.load();
+    }
+}
